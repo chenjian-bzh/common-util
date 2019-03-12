79450d54b18d476200dea2c3c4ba1933b6aa17c8
陈健 <chenjian.bzh@bytedance.com>
2019-03-12 11:32:07 +0800

    feat: add hmac method

--- src/crypto/index.js
+++ src/crypto/index.js
@@ -1,24 +1,48 @@
 import crypto from 'crypto';
-import debug from '../debug'
-//character encodings supported by nodejs v10.x
-const Encoding_Method = ['hex', 'base64', 'ascii', 'utf8', 'utf16le', 'ucs2', 'latin1', 'binary']
-const logger = debug('util:crypto')
+import assert from 'assert';
 
+//character encodings supported by nodejs v10.x
+const ENCODING = ['hex', 'base64', 'ascii', 'utf8', 'utf16le', 'ucs2', 'latin1', 'binary']
+
+/**
+ * @param  {} alg - 摘要算法
+ * @param  {} s - 消息
+ * @param  {} encoding - 摘要编码方式
+ */
 export function hash(alg, s, encoding = 'hex') {
     const h = crypto.createHash(alg);
-    if (!Encoding_Method.includes(encoding)) {
-        logger.warn('character encoding is not supported by nodejs!');
-    };
+    assert(ENCODING.includes(encoding), 'character encoding is not supported by nodejs!');
+
     const isBuffer = Buffer.isBuffer(s);
     if (!isBuffer && typeof s === 'object') {
         s = JSON.stringify(s);
     }
     h.update(s, isBuffer ? undefined : 'utf-8');
-    const digest = h.digest(s, encoding)
-    logger.log(digest);
+
+    const digest = h.digest(encoding);
     return digest;
 }
 
 export function md5(s, encoding) {
     return hash('md5', s, encoding);
+}
+
+export function sha1(s, encoding) {
+    return hash('sha1', s, encoding);
+}
+
+export function hmac(alg, secret, s, encoding = 'base64') {
+    const h = crypto.createHmac(alg, secret);
+    assert(ENCODING.includes(encoding), 'character encoding is not supported by nodejs!');
+
+    const isBuffer = Buffer.isBuffer(s);
+    if (!isBuffer && typeof s === 'object') {
+        s = JSON.stringify(s);
+    }
+
+    h.update(s, isBuffer ? undefined : 'utf-8');
+
+    const digest = h.digest(encoding);
+    console.log(digest)
+    return digest;
 }
--- src/crypto/index.test.js
+++ src/crypto/index.test.js
@@ -1,9 +1,17 @@
-import { md5, hash } from './index';
+import { md5, sha1, hmac } from './index';
 
 describe('crypto', () => {
-    describe('md5', () => {
+    describe('hash', () => {
         test('digest string by md5', () => {
             expect(md5('hello world')).toBeDefined();
+        });
+        test('digest string by sha1', () => {
+            expect(sha1('hello world')).toBeDefined();
+        })
+    });
+    describe('hmac', () => {
+        test('test hmac', () => {
+            expect(hmac('md5', 'a secret', 'hello world')).toBeDefined();
         })
     })
 })
